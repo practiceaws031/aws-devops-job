ad83dfb4110cfa5f9988abd32d086554c8073262
satheesh <satheesht676@gmail.com>
2022-09-08 20:38:33 +0530

    changes 1

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "installing terraform"
 sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 echo "terraform installation completed"
--- ssh_key.sh
+++ ssh_key.sh
@@ -2,5 +2,5 @@
 set -e
 echo "gathering git hub ssh key"
 ssh-keyscan -t rsa github.com>> $HOME/.ssh/known_hosts
-cp private_key $HOME/.ssh/id_rsa
-chmod 600 $HOME/.ssh/id_rsa
+#cp private_key $HOME/.ssh/id_rsa
+#chmod 600 $HOME/.ssh/id_rsa
